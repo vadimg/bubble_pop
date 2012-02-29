cc95ebacda1faa59a8c7fae177060a2f0918d065
Vadim Graboys <dimva13@gmail.com>
2012-02-28 22:57:48 -0500

    change plot.py to work with async io

--- plot.py
+++ plot.py
@@ -1,3 +1,4 @@
+import tornado.ioloop
 import math
 import matplotlib.pyplot as plt
 from rates import getZeroData
@@ -6,14 +7,15 @@ from calcs import interpolate, makeForward, makeProb
 YEARS = 7
 PROB_SCALE = 5
 
+def process(rates, update_date):
+    irates = interpolate(rates)
+    forward = makeForward(YEARS*12, irates)
+    prob = makeProb(PROB_SCALE, forward)
 
-rates = getZeroData()
+    x = range(1,20*12+1)
+    plt.plot(x, map(irates, x))
+    #plt.show()
 
-irates = interpolate(rates)
-forward = makeForward(YEARS*12, irates)
-prob = makeProb(PROB_SCALE, forward)
 
-x = range(1,20*12+1)
-plt.plot(x, map(irates, x))
-plt.show()
-
+getZeroData(process)
+tornado.ioloop.IOLoop.instance().start()
